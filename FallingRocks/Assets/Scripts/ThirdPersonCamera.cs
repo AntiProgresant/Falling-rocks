d1259f5f6cdb9a4def1c8095c8ea5d129cee91a9
AntiProgresant <51537771+AntiProgresant@users.noreply.github.com>
2020-11-30 22:17:14 -0500

    First Commit

---
+++ FallingRocks/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public bool lockCursor;
+    public float mouseSensitivity = 10;
+    public Transform target;
+    public float dstFromTarget;
+    public Vector2 pitchMinMax = new Vector2(-49, 85);
+
+    public float RotationSmoothTime = .12f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    float pitch;
+    float yaw;
+
+    void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch += Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y); //Clamps camera agle using Vector2 values
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, RotationSmoothTime);
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * dstFromTarget;
+    }
+}
---
+++ FallingRocks/Assets/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class Score : MonoBehaviour
+{
+    public Text timerText;
+    private float timeElapsed = 0f;
+
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        timerText.text = "Time: " + FormatTime(timeElapsed);
+    }
+
+    string FormatTime(float value)
+    {
+        TimeSpan t = TimeSpan.FromSeconds(value);
+        return string.Format("{0.D2}:{1:D2}", t.Minutes, t.Seconds);
+    }
+}
