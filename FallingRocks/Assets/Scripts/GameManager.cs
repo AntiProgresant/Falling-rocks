508de5d1dcf56bfb1bd70f050f7b821a27c6c00e
AntiProgresant <alexanderlane614@gmail.com>
2021-07-05 23:07:50 -0400

    Main Menu

--- FallingRocks/Assets/Scripts/GameManager.cs
+++ FallingRocks/Assets/Scripts/GameManager.cs
@@ -6,9 +6,10 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
+    
     public void EndGame()
     {
-        if (gameHasEnded == false)
+        if (gameHasEnded == true)
         {
             gameHasEnded = true;
             Debug.Log("Game Over");
